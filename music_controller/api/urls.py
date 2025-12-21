0b7bf0fb85c058fa92573ca3490ff3986587152e
Adelana107 <adelana787898@gmail.com>
2025-12-21 18:47:39 +0100

    handling POST Request (Django Rest)

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,9 @@
+from venv import create
 from django.urls import path   
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 
 urlpatterns = [
-    path("room", RoomView.as_view())
+    path("room", RoomView.as_view()),
+    path("create-room", CreateRoomView.as_view())
 ]
